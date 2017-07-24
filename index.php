861458cb0907c9e26faaaf3b65cc233b58dc23e3
Aria Chen <964385217@qq.com>
2017-07-24 22:18:25 +0800

    增加用户输入分享码

--- index.php
+++ index.php
@@ -5,11 +5,9 @@ session_start ();
 	/**
 	 * 【php获取目录中的所有文件名】
 	 */
-
 	//1、先打开要操作的目录，并用一个变量指向它
 	//打开当前目录下的目录pic下的子目录common。
 $handler = opendir('./upload');
-
 //2、循环的读取目录下的所有文件
 //其中$filename = readdir($handler)是每次循环的时候将读取的文件名赋值给$filename，为了不陷于死循环，所以还要让$filename !== false。一定要用!==，因为如果某个文件名如果叫’0′，或者某些被系统认为是代表false，用!=就会停止循环*/
 echo "<h1>所有文件</h1>";
@@ -41,7 +39,6 @@ closedir($handler);
 
 <?php
 header('Content-Type:text/html;charset=utf-8');
-
 //echo $_SESSION['islogin'];
 if (!isset ($_SESSION['islogin'])){
 echo "</br>";
@@ -65,6 +62,7 @@ exit () ;
 </br>
 <form action="upload.php" method="post" enctype="multipart/form-data">
 上传文件：<input name="upload_file" type="file" /><br/>
+文件分享码:<input type="password" name="baseKey"/><br/>
 <input type="submit" value="上传" />
 <a href='https://rachelaria.com/logout.php'>退出登录</a>
 </br>
