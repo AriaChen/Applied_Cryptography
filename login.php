6a03191814286ec59f21a7a9a0698748453814d7
Aria Chen <964385217@qq.com>
2017-07-25 11:11:04 +0800

    增加css样式

--- login.php
+++ login.php
@@ -1,9 +1,46 @@
-    <form action="logincheck.php" method="post">  
-        用户名：<input type="text" name="username" />  
-        <br />  
-        密码：<input type="password" name="password" />  
-        <br />  
-        <input type="submit" name="submit" value="登陆" />  
-              
-        <a href="register.php">注册</a>  
-    </form>  
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./bootstrap/css/signin.css" rel="stylesheet">
+
+
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" action="logincheck.php" method="post">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputUsername" class="sr-only">用户名</label>
+        <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+        <label for="inputPassword" class="sr-only">密码</label>
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">登录</button>
+	<br>
+ 	<p><font size = "4">还没有账号？立即<a href="register.php">注册</a> </font> </p>
+
+      </form>
+
+    </div> <!-- /container -->
+
+
+  </body>
+</html>
+
+
