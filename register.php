18be7812727e13c4990208e50a79306b671c77aa
Aria Chen <964385217@qq.com>
2017-07-25 11:26:24 +0800

    增加样式

--- register.php
+++ register.php
@@ -1,9 +1,43 @@
-    <form action="regcheck.php" method="post">  
-        用户名：<input type="text" name="username"/>  
-        <br/>  
-        密　码:<input type="password" name="password"/>  
-        <br/>  
-        确认密码：<input type="password" name="confirm"/>  
-        <br/>  
-        <input type="Submit" name="Submit" value="注册"/>  
-    </form>  
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./bootstrap/css/signin.css" rel="stylesheet">
+
+
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <form class="form-signin" action="logincheck.php" method="post">
+        <h2 class="form-signin-heading">Please sign in</h2>
+        <label for="inputUsername" class="sr-only">用户名</label>
+        <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+        <label for="inputPassword" class="sr-only">密码</label>
+        <input type="password" name="password" class="form-control" placeholder="Password" required>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">登录</button>
+	<br>
+ 	<p><font size = "4">还没有账号？立即<a href="register.php">注册</a> </font> </p>
+
+      </form>
+
+    </div> <!-- /container -->
+
+
+  </body>
+</html>
+
+
