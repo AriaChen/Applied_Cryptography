bc4fae86ee97aba7245117f090dfce8f89f6d989
Aria Chen <964385217@qq.com>
2017-07-27 11:45:04 +0800

    Create regcheck.php

--- regcheck.php
+++ regcheck.php
@@ -79,26 +79,22 @@
                   if($psw == $psw_confirm)
                   {
 
-  		        $mysqli = new mysqli("localhost", "root", $_SERVER['MYSQL_PSW'],"login");
+  		      $mysqli = new mysqli("localhost", "root", $_SERVER['MYSQL_PSW'],"login");
 
-      		            if(!$mysqli)  {
+      		      if(!$mysqli) 
                           echo"数据库连接失败";
-      	   	          }
-                      else{
-         		              echo"数据库连接成功";
-    		              }
-  		                $sql = "select uname from users where uname = '$_POST[username]'"; //SQL语句
+ 
+  		      $sql = "select uname from users where uname = '$_POST[username]'"; //SQL语句
                       $result = $mysqli->query($sql);    //执行SQL语句
                       $num = mysqli_num_rows($result);
                       //统计执行结果影响的行数
 
-  		                if($num)    //如果已经存在该用户
-                      {
+  		      if($num)    //如果已经存在该用户
                           echo "<script>alert('用户名已存在'); history.go(-1);</script>";
-                      }
+                      
                       else    //不存在当前注册用户名称
                       {
-  			                   //Message to be hashed.password.
+  			   //Message to be hashed.password.
                            $data = $_POST["password"];
                            $enpsw= password_hash ($data, PASSWORD_DEFAULT);
 			 
@@ -115,23 +111,18 @@
                               echo "<script>alert('注册成功！');window.location.href='https://rachelaria.com/login.php'</script>";
                            }
                            else
-                           {
                               echo "<script>alert('系统繁忙，请稍候！'); history.go(-1);</script>";
-                           }
                       }
 
                     }
                     else
-                    {
                       echo "<script>alert('密码不一致！'); history.go(-1);</script>";
-                    }
+
               }
 
             }
         }
         else
-        {
             echo "<script>alert('提交未成功！'); history.go(-1);</script>";
-        }
 
     ?>
